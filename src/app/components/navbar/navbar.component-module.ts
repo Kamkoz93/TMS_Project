8957972174b814af88730a76b02b04d39e9f7815
Kamkoz <kamkoz93@gmail.com>
2023-05-17 14:57:41 +0200

    feat: navbar with slot crateted

---
+++ src/app/components/navbar/navbar.component-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  imports: [],
+  declarations: [],
+  providers: [],
+  exports: [],
+})
+export class NavbarComponentModule {}
