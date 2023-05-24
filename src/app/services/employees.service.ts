439e13423a4453650a0df4c7e42a41e0292471a3
Kamkoz <kamkoz93@gmail.com>
2023-05-24 10:51:06 +0200

    feat: improved getOne methods

--- src/app/services/employees.service.ts
+++ src/app/services/employees.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map, shareReplay } from 'rxjs';
+import { Observable, map, shareReplay, tap } from 'rxjs';
 import { EmployeeModel } from '../models/employee.model';
 
 @Injectable({ providedIn: 'root' })
@@ -8,19 +8,19 @@ export class EmployeesService {
   constructor(private _httpClient: HttpClient) {}
 
   getAll(): Observable<EmployeeModel[]> {
-    return this._httpClient
-      .get<EmployeeModel[]>(
-        'https://63810e439440b61b0d10b7c7.mockapi.io/employees'
-      )
-      .pipe(
-        map((data) => data),
-        shareReplay(1)
-      );
+    return this._httpClient.get<EmployeeModel[]>(
+      'https://63810e439440b61b0d10b7c7.mockapi.io/employees'
+    );
   }
 
   getOne(id: string): Observable<EmployeeModel> {
     return this.getAll().pipe(
-      map((employees) => employees.filter((emp) => emp.id === id)[0])
+      map((employees) => employees.filter((emp) => emp.id === id)[0]),
+      tap((emp) => {
+        if (!emp) {
+          throw new Error('Employee not found');
+        }
+      })
     );
   }
 }
--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TeamModel } from '../models/team.model';
-import { Observable, combineLatest, map } from 'rxjs';
+import { Observable, combineLatest, map, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
@@ -14,7 +14,12 @@ export class TeamsService {
 
   getOne(id: string): Observable<TeamModel> {
     return this.getAll().pipe(
-      map((teams) => teams.filter((team) => team.id === id)[0])
+      map((teams) => teams.filter((team) => team.id === id)[0]),
+      tap((team) => {
+        if (!team) {
+          throw new Error('Team not found');
+        }
+      })
     );
   }
 }
