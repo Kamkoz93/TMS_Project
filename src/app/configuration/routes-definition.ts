e156b8fa9990e6cc156e5d2c6278e54be52ae78e
Kamkoz <kamkoz93@gmail.com>
2023-05-26 09:33:26 +0200

    feat: updated routes

--- src/app/configuration/routes-definition.ts
+++ src/app/configuration/routes-definition.ts
@@ -8,9 +8,14 @@ export enum ROUTES_DEF {
 }
 
 export enum API_ROUTES_DEF {
-  EMPLOYEES = 'https://63810e439440b61b0d10b7c7.mockapi.io/employees',
-  TEAMS = 'https://6384fca14ce192ac60696c4b.mockapi.io/teams',
-  PROJECTS = 'https://6384fca14ce192ac60696c4b.mockapi.io/projects',
+  // EMPLOYEES = 'https://63810e439440b61b0d10b7c7.mockapi.io/employees',
+  // TEAMS = 'https://6384fca14ce192ac60696c4b.mockapi.io/teams',
+  // PROJECTS = 'https://6384fca14ce192ac60696c4b.mockapi.io/projects',
+  // TASKS = 'https://63810e439440b61b0d10b7c7.mockapi.io/tasks',
+  // CHECKLIST_ITEMS = 'https://63810e439440b61b0d10b7c7.mockapi.io/checklist-items',
+  EMPLOYEES = 'https://646f899709ff19b120876fb8.mockapi.io/employees',
+  TEAMS = 'https://646f899709ff19b120876fb8.mockapi.io/teams',
+  PROJECTS = 'https://646f899709ff19b120876fb8.mockapi.io/projects',
   TASKS = 'https://63810e439440b61b0d10b7c7.mockapi.io/tasks',
   CHECKLIST_ITEMS = 'https://63810e439440b61b0d10b7c7.mockapi.io/checklist-items',
 }
