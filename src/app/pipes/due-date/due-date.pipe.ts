aee0307881d0ecf1aedcf6fd130cb7f85222cebd
Kamkoz <kamkoz93@gmail.com>
2023-06-12 09:58:12 +0200

    feat: improved pipe, removed imported date pipe

--- src/app/pipes/due-date/due-date.pipe.ts
+++ src/app/pipes/due-date/due-date.pipe.ts
@@ -1,24 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { DatePipe } from '@angular/common';
 
 @Pipe({
   name: 'dueDate',
 })
 export class DueDatePipe implements PipeTransform {
-  constructor(private datePipe: DatePipe) {}
-
-  transform(value: string | number): string {
-    let date: Date;
-
-    if (typeof value === 'string') {
-      date = new Date(value);
-    } else if (typeof value === 'number') {
-      date = new Date(value * 1000);
-    } else {
-      return '';
+  transform(value: string | number | null): string {
+    if (typeof value === 'number') {
+      const date = new Date(value * 1000);
+      const month = date.toLocaleString('en', { month: 'short' });
+      const day = date.getDate();
+      const year = date.getFullYear();
+      return `Due on ${month} ${day}, ${year}`;
     }
 
-    date.setDate(date.getDate() - 1);
-    return 'Due on ' + this.datePipe.transform(date, 'MMM d, y');
+    return 'Due on ' + value;
   }
 }
